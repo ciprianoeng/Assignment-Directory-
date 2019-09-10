ed1bb7eccf2de50aca89c5101fafa934b2c8a252
ciprianoeng <47126931+ciprianoeng@users.noreply.github.com>
2019-09-10 12:57:39 -0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World!\n" + "My name is Carlos Cipriano.\v" + "This is my first Python program!\v" + "Have a nice day.")
+Hello World!
+My name is Carlos Cipriano.This is my first Python program!Have a nice day.
+>>> 
