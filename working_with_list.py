0a7bf46ef4b1d4641a623faf4f57e06e97e16544
ciprianoeng <47126931+ciprianoeng@users.noreply.github.com>
2019-09-16 00:20:39 -0500

    Update working_with_list.py

--- working_with_list.py
+++ working_with_list.py
@@ -1,4 +1,17 @@
-Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+>> grocery_store_items = ['Apple', 'Orange', 'Steak', 'Chicken', 'Water']
+>> grocery_item_price = ['10', '20', '30', '40', '50']
+>> print(grocery_store_items[2], grocery_item_price[2])
+>> Steak 30
+>> print(grocery_store_items[-1], grocery_item_price[-1])
+>> Water 50
+>> print(grocery_store_items.insert[-1, 'Paneapple'], grocery_item_price.insert[-1, '60'])
+>> print(grocery_store_items)
+>> ['Apple', 'Orange', 'Steak', 'Chicken', 'Water', 'Paneapple']
+>> print(grocery_item_price)
+>> ['10', '20', '30', '40', '50', "60"]
+>> print(grocery_store_items.remove(0), grocery_item_price.remove(0))
+>> print(grocery_item_price.insert(1, rocery_item_price[]*2)
+>> print(grocery_store_items)
+>> ['Orange', 'Steak', 'Chicken', 'Water', 'Paneapple']
+>> print(grocery_item_price)
+>> ['20', '60', '40', '50', "60"]
