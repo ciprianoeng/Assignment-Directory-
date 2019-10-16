9a2084f12e4b9833d3e653aac45e913a1002b175
ciprianoeng <47126931+ciprianoeng@users.noreply.github.com>
2019-10-16 12:59:12 -0500

    Delete working_with_variables.py

--- assignemet_2/working_with_variables.py
+++
@@ -1,9 +0,0 @@
-Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a = "Symbol:AAPL, Price:204.66, Change:-4.08\v"
->>> b = "Symbol:aapl, Price:$204.66, Change:-4.08\v"
->>> c = "Symbol:AAPL---Yesterday's Price:200:58"
->>> print(a, b, c)
-Symbol:AAPL, Price:204.66, Change:-4.08 Symbol:aapl, Price:$204.66, Change:-4.08 Symbol:AAPL---Yesterday's Price:200:58
->>> 
