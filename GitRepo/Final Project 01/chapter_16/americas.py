7b2ad7bcdb6ab0a0d3b3befb7ef6a64c03c83f2c
ciprianoeng <47126931+ciprianoeng@users.noreply.github.com>
2019-12-17 21:31:32 -0600

    Add files via upload

---
+++ GitRepo/Final Project 01/chapter_16/americas.py
@@ -0,0 +1,15 @@
+#Carlos Cipriano
+#ID: 0719076
+#12/16/2019
+
+import pygal
+
+wm = pygal.Worldmap()
+wm.title = 'North, Central, and South America'
+
+wm.add('North America', ['ca', 'mx', 'us'])
+wm.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'pa', 'sv'])
+wm.add('South America', ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'gf',
+    'gy', 'pe', 'py', 'sr', 'uy', 've'])
+    
+wm.render_to_file('americas.svg')
